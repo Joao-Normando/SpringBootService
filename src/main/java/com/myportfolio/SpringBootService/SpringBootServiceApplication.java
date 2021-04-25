a17e0a1375c6befad5ef55548334bbcf9c6e0587
Joao-Normando <joao.normando@maisunifacisa.com.br>
2021-04-25 15:13:06 -0300

    Projeto Criado

---
+++ src/main/java/com/myportfolio/SpringBootService/SpringBootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myportfolio.SpringBootService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServiceApplication.class, args);
+	}
+
+}
